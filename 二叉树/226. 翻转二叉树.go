fc60278183b0575f19b6c927c12c8af864857f1f
renqiming03 <renqiming03@meituan.com>
2023-08-10 18:37:53 +0800

    day02: 二叉树

---
+++ 二叉树/226. 翻转二叉树.go
@@ -0,0 +1,17 @@
+package main
+
+import "container/list"
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+
+func invertTree2(root *TreeNode) *TreeNode {
+
+}
