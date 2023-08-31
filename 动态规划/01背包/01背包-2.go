15997a90fb91e30e1f20fd51f064ab784a7c2268
darkTang <2223191690@qq.com>
2023-09-01 00:13:05 +0800

    day14

--- 动态规划/01背包/01背包-2.go
+++ 动态规划/01背包/01背包-2.go
@@ -5,49 +5,10 @@ import (
 )
 
 func main() {
-	//bagProblem5()
-	//weight := []int{7, 3, 4, 2}
-	//value := []int{3, 6, 9, 1, 2, 4, 10, 1, 3}
-
-	//sort.Slice(value, func(i, j int) bool {
-	//	weight[i], weight[j] = weight[j], weight[i]
-	//	return value[i] < value[j]
-	//})
-	//
-	//fmt.Println(weight)
-	//fmt.Println(value)
-	//fmt.Println(quickSort(value))
-
-	v := make([]int, 1, 2)
-	fmt.Printf("%p\n", &v)
-	fmt.Printf("%p\n", v)
-	s := append(v, 1)
-	fmt.Printf("%p\n", &s)
-	fmt.Printf("%p\n", s)
-	v = append(v, 1)
-	fmt.Printf("%p\n", &v)
-	fmt.Printf("%p\n", v)
-
+	bagProblem7()
 }
 
-func quickSort(value []int) []int {
-	if len(value) <= 1 {
-		return value
-	}
-	var left []int
-	var right []int
-	pivot := value[0]
-	for i := 1; i < len(value); i++ {
-		if value[i] >= pivot {
-			right = append(right, value[i])
-		} else {
-			left = append(left, value[i])
-		}
-	}
-	return append(quickSort(left), append([]int{pivot}, quickSort(right)...)...)
-
-}
-
+// 贪心算法实现
 func bagProblem5() {
 	var m int
 	var n int
@@ -92,7 +53,6 @@ func bagProblem5() {
 	}
 	fmt.Println(maxValue)
 }
-
 func bubbleSort(weight, value []int) {
 	for i := 0; i < len(value)-1; i++ {
 		for j := 0; j < len(value)-i-1; j++ {
@@ -104,12 +64,110 @@ func bubbleSort(weight, value []int) {
 	}
 }
 
-/*
-	10 4
-	7  6
-	3  3
-	4  5
-	2  2
-*/
+// 二维dp数组实现 dp[i][j] i 表示取 0-i 任意物品，j 表示 背包重量
+func bagProblem6() {
+	var m int
+	var n int
+	_, _ = fmt.Scan(&m, &n)
+	weight := make([]int, n)
+	value := make([]int, n)
+	for i := 0; i < n; i++ {
+		var w int
+		var v int
+		_, _ = fmt.Scan(&w, &v)
+		weight[i] = w
+		value[i] = v
+	}
 
-// 10
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, m+1)
+	}
+
+	// 初始化dp数组
+	for i := 0; i <= m; i++ {
+		if i >= weight[0] {
+			dp[0][i] = value[0]
+		}
+	}
+
+	for i := 1; i < n; i++ {
+		for j := 1; j <= m; j++ {
+			if j < weight[i] {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = maxNum2(dp[i-1][j], dp[i-1][j-weight[i]]+value[i])
+			}
+		}
+	}
+	fmt.Println("最大价值：")
+	fmt.Println(dp[n-1][m])
+	fmt.Println("dp数组：")
+	for i := 0; i < n; i++ {
+		for j := 0; j <= m; j++ {
+			fmt.Printf("%d  ", dp[i][j])
+		}
+		fmt.Println()
+	}
+}
+
+// 一维dp数组实现 dp[j] j 表示 背包重量
+func bagProblem7() {
+	var m int
+	var n int
+	_, _ = fmt.Scan(&m, &n)
+	weight := make([]int, n)
+	value := make([]int, n)
+	for i := 0; i < n; i++ {
+		var w int
+		var v int
+		_, _ = fmt.Scan(&w, &v)
+		weight[i] = w
+		value[i] = v
+	}
+
+	dp := make([]int, m+1)
+	for i := 0; i < n; i++ {
+		for j := m; j >= 0; j-- {
+			if j >= weight[i] {
+				dp[j] = maxNum2(dp[j], dp[j-weight[i]]+value[i])
+			}
+		}
+		// 输出每次的dp数组
+		for k := 0; k <= m; k++ {
+			fmt.Printf("%d  ", dp[k])
+		}
+		fmt.Println()
+	}
+	fmt.Println(dp[m])
+
+}
+
+func maxNum2(x, y int) int {
+	if x >= y {
+		return x
+	} else {
+		return y
+	}
+}
+
+/*
+   【输入】
+		10 4
+		7  6
+		3  3
+		4  5
+		2  2
+    【输出】
+		10
+*/
+/*
+   【输入】
+		10 4
+		2  1
+		3  3
+		4  5
+		7  9
+    【输出】
+		12
+*/
